05cb64b13f5b188a05209db11112245135d0a9af
s900207 <s90020750122@gmail.com>
2024-05-17 11:51:14 +0800

    更正

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,5 @@
 <template>
-  <div id="app">
-    <head>
-      <!-- 预加载字体 -->
-      <link rel="preload" href="https://s900207.github.io/book-front/assets/materialdesignicons-webfont-48d3eec6.woff" as="font" type="font/woff" crossorigin="anonymous">
-    </head>
-    <router-view></router-view>
-  </div>
+<v-app>
+  <router-view></router-view>
+</v-app>
 </template>
-
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
-<style>
-@font-face {
-  font-family: 'MaterialDesignIcons';
-  src: url('https://s900207.github.io/book-front/assets/materialdesignicons-webfont-48d3eec6.woff') format('woff');
-  font-weight: normal;
-  font-style: normal;
-  font-display: swap;
-}
-
-body {
-  font-family: 'MaterialDesignIcons', sans-serif;
-}
-</style>
