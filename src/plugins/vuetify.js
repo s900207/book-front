3bc61e2075c01994e1b9e6c289ba072b80e65bbf
s900207 <s90020750122@gmail.com>
2024-09-18 22:02:27 +0800

    測試

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,6 +3,7 @@
  *
  * Framework documentation: https://vuetifyjs.com`
  */
+
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
@@ -11,10 +12,6 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 import { zhHant } from 'vuetify/locale'
-document.addEventListener('DOMContentLoaded', () => {
-  const preloadLinks = document.querySelectorAll('link[rel="preload"][as="font"][type="font/eot"]')
-  preloadLinks.forEach(link => link.remove())
-})
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
