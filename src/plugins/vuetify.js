eeb0da77dbc8b7dbae9fc5bebde6a71398c873fc
s900207 <s90020750122@gmail.com>
2024-09-18 21:36:11 +0800

    測試

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,6 @@
  *
  * Framework documentation: https://vuetifyjs.com`
  */
-
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
@@ -12,6 +11,19 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 import { zhHant } from 'vuetify/locale'
+const preloadFont = (href, type) => {
+  const link = document.createElement('link')
+  link.rel = 'preload'
+  link.href = href
+  link.as = 'font'
+  link.type = type
+  link.crossOrigin = 'anonymous'
+  document.head.appendChild(link)
+}
+
+preloadFont('https://s900207.github.io/book-front/assets/materialdesignicons-webfont-61e8aba5.ttf', 'font/ttf')
+preloadFont('https://s900207.github.io/book-front/assets/materialdesignicons-webfont-a5928a0d.woff', 'font/woff')
+preloadFont('https://s900207.github.io/book-front/assets/materialdesignicons-webfont-662fefa8.woff2', 'font/woff2')
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
