9c8651509df71080b0717be39f5606a9f0fe82d9
s900207 <s90020750122@gmail.com>
2024-09-23 23:18:32 +0800

    測試

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -7,6 +7,7 @@ VContainer
           menu-icon=""
           placeholder="請輸入書籍名稱"
           v-model="searchTerm"
+          svgIcon="icon-magnify"
         )
           template(v-slot:append)
             svgIcon(href="#icon-magnify")
